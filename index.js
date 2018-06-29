82e26b2e631847cec31c8fd946e9df6926431df5
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-06-29 05:09:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function johnLennonFacts (facts) {
 }
 function iLoveTheBeatles(n) {
   var array = [];
-  do { array.push(' I love the Beatles!');
+  do { array.push('I love the Beatles!');
   n++
  } while ( n < 15)
   return array
