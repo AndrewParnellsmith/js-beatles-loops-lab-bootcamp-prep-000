0a9b22e9ec851525e3de4868e7dbe3b9d781b262
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-06-29 03:51:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instrument) {
+ var theBeatles = [];
+ for ( var i = 0; i < musicians.length; i++  ) {
+   theBeatles.push(musicians[i]  + ' plays ' + instrument[i]);
+ }
+ return theBeatles;
+}
+function johnLennonFacts () {
+ var facts = 
+  ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice" ];
+  
+  while( array.length is 5)
+}
