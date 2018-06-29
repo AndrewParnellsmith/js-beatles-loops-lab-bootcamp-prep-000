25e37a225b38d3fa2f5bc23f1f9c0310729ffee8
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-06-29 04:23:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function johnLennonFacts () {
   ["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice", "foo", "bar" ];
+  "He hated the sound of his own voice", ];
   var i = 0;
   var newFacts = []
   while( i < facts.length) { 
